9328788289f6a7be64942f8be22ca3ccb41cf6e8
Paul <pauljoshyk96@gmail.com>
2016-06-18 18:45:53 +0530

    console detects drag now

--- js/dom.js
+++ js/dom.js
@@ -77,11 +77,35 @@ function generateOnLoad()
     }
     
     //generate slider
-    $(tableDoms[0]).after('<br><div id="slider" draggable = "true"><div>srgef</div></div>');
+    $(tableDoms[0]).after('<br><div id="slider" draggable = "true" ondrag = "logger()"><div>srgef</div></div>');
+    function logger( ev )
+    {
+        console.log("efef");
+    }
 }
 
 
+/*function moveSlider()
+{
+    var curxpos =0;
+    var curypos = 0;
+    var curdown = false;
+    var sliderDom = document.getElementById('slider');
+    
+    sliderDom.addEventListener('mousemove', function(e)
+    { 
+        if(curDown === true)
+        {
+            sliderDom.scrollTo( sliderDom.scrollTop + (curYPos - e.pageY));
+        }
+    });
+
+    window.addEventListener('mousedown', function(e){ curDown = true; curYPos = e.pageY; curXPos = e.pageX; });
+    window.addEventListener('mouseup', function(e){ curDown = false; });
+
+}*/
 
 window.onload = function(){
     getDetails();
+    //moveSlider();
 };
